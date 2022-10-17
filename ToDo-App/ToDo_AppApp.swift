90fba0a07d0c93eab3e92f5359833bde138ec8fc
Mateusz Rybczynski <mateusz.rybczynski@icloud.com>
2022-10-17 17:36:56 +0200

    Initial Commit

---
+++ ToDo-App/ToDo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_AppApp.swift
+//  ToDo-App
+//
+//  Created by Mateusz Rybczyński on 17/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
